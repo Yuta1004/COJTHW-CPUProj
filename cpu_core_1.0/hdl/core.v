98bc9b1fcb282a1752ce91e616a624d81e588cdd
NakagamiYuta <guguru0014@gmail.com>
2022-12-07 15:34:21 +0900

    [add](core) M_DATA_AXIポート追加

--- cpu_core_1.0/hdl/core.v
+++ cpu_core_1.0/hdl/core.v
@@ -69,6 +69,59 @@ module core #
         input  wire                                 M_INST_AXI_RVALID,
         output wire                                 M_INST_AXI_RREADY,
 
+        /* ----- AXIバス信号(データ用) ----- */
+        // AWチャンネル
+        output wire [C_M_AXI_THREAD_ID_WIDTH-1:0]   M_DATA_AXI_AWID,
+        output wire [C_M_AXI_ADDR_WIDTH-1:0]        M_DATA_AXI_AWADDR,
+        output wire [8-1:0]                         M_DATA_AXI_AWLEN,
+        output wire [3-1:0]                         M_DATA_AXI_AWSIZE,
+        output wire [2-1:0]                         M_DATA_AXI_AWBURST,
+        output wire [2-1:0]                         M_DATA_AXI_AWLOCK,
+        output wire [4-1:0]                         M_DATA_AXI_AWCACHE,
+        output wire [3-1:0]                         M_DATA_AXI_AWPROT,
+        output wire [4-1:0]                         M_DATA_AXI_AWQOS,
+        output wire [C_M_AXI_AWUSER_WIDTH-1:0]      M_DATA_AXI_AWUSER,
+        output wire                                 M_DATA_AXI_AWVALID,
+        input  wire                                 M_DATA_AXI_AWREADY,
+
+        // Wチャンネル
+        output wire [C_M_AXI_DATA_WIDTH-1:0]        M_DATA_AXI_WDATA,
+        output wire [C_M_AXI_DATA_WIDTH/8-1:0]      M_DATA_AXI_WSTRB,
+        output wire                                 M_DATA_AXI_WLAST,
+        output wire [C_M_AXI_WUSER_WIDTH-1:0]       M_DATA_AXI_WUSER,
+        output wire                                 M_DATA_AXI_WVALID,
+        input  wire                                 M_DATA_AXI_WREADY,
+
+        // Bチャンネル
+        input  wire [C_M_AXI_THREAD_ID_WIDTH-1:0]   M_DATA_AXI_BID,
+        input  wire [2-1:0]                         M_DATA_AXI_BRESP,
+        input  wire [C_M_AXI_BUSER_WIDTH-1:0]       M_DATA_AXI_BUSER,
+        input  wire                                 M_DATA_AXI_BVALID,
+        output wire                                 M_DATA_AXI_BREADY,
+
+        // ARチャンネル
+        output wire [C_M_AXI_THREAD_ID_WIDTH-1:0]   M_DATA_AXI_ARID,
+        output wire [C_M_AXI_ADDR_WIDTH-1:0]        M_DATA_AXI_ARADDR,
+        output wire [8-1:0]                         M_DATA_AXI_ARLEN,
+        output wire [3-1:0]                         M_DATA_AXI_ARSIZE,
+        output wire [2-1:0]                         M_DATA_AXI_ARBURST,
+        output wire [2-1:0]                         M_DATA_AXI_ARLOCK,
+        output wire [4-1:0]                         M_DATA_AXI_ARCACHE,
+        output wire [3-1:0]                         M_DATA_AXI_ARPROT,
+        output wire [4-1:0]                         M_DATA_AXI_ARQOS,
+        output wire [C_M_AXI_ARUSER_WIDTH-1:0]      M_DATA_AXI_ARUSER,
+        output wire                                 M_DATA_AXI_ARVALID,
+        input  wire                                 M_DATA_AXI_ARREADY,
+
+        // Rチャンネル
+        input  wire [C_M_AXI_THREAD_ID_WIDTH-1:0]   M_DATA_AXI_RID,
+        input  wire [C_M_AXI_DATA_WIDTH-1:0]        M_DATA_AXI_RDATA,
+        input  wire [2-1:0]                         M_DATA_AXI_RRESP,
+        input  wire                                 M_DATA_AXI_RLAST,
+        input  wire [C_M_AXI_RUSER_WIDTH-1:0]       M_DATA_AXI_RUSER,
+        input  wire                                 M_DATA_AXI_RVALID,
+        output wire                                 M_DATA_AXI_RREADY,
+
         /*----- CPU制御信号 ----- */
         // クロック
         input wire          CCLK,
@@ -114,7 +167,7 @@ module core #
         output wire [31:0]  REGPC
     );
 
-    /* ----- AXIバス設定 ----- */
+    /* ----- AXIバス設定(命令用) ----- */
     // AWチャンネル
     assign M_INST_AXI_AWID      = 'b0;
     assign M_INST_AXI_AWADDR    = 32'b0;   // *
@@ -153,6 +206,46 @@ module core #
 
     // Rチャンネル
     assign M_INST_AXI_RREADY    = 1'b0;    // *
+
+    /* ----- AXIバス設定(データ用) ----- */
+    // AWチャンネル
+    assign M_DATA_AXI_AWID      = 'b0;
+    assign M_DATA_AXI_AWADDR    = 32'b0;   // *
+    assign M_DATA_AXI_AWLEN     = 8'b0;    // *
+    assign M_DATA_AXI_AWSIZE    = 3'b010;
+    assign M_DATA_AXI_AWBURST   = 2'b01;
+    assign M_DATA_AXI_AWLOCK    = 2'b00;
+    assign M_DATA_AXI_AWCACHE   = 4'b0011;
+    assign M_DATA_AXI_AWPROT    = 3'h0;
+    assign M_DATA_AXI_AWQOS     = 4'h0;
+    assign M_DATA_AXI_AWUSER    = 'b0;
+    assign M_DATA_AXI_AWVALID   = 1'b0;    // *
+
+    // Wチャンネル
+    assign M_DATA_AXI_WDATA     = 32'b0;   // *
+    assign M_DATA_AXI_WSTRB     = 4'b1111;
+    assign M_DATA_AXI_WLAST     = 1'b0;    // *
+    assign M_DATA_AXI_WUSER     = 'b0;
+    assign M_DATA_AXI_WVALID    = 1'b0;     // *
+
+    // Bチャンネル
+    assign M_DATA_AXI_BREADY    = 1'b0;     // *
+
+    // ARチャンネル
+    assign M_DATA_AXI_ARID      = 'b0;
+    assign M_DATA_AXI_ARADDR    = 32'b0;   // *
+    assign M_DATA_AXI_ARLEN     = 8'b0;    // *
+    assign M_DATA_AXI_ARSIZE    = 3'b010;
+    assign M_DATA_AXI_ARBURST   = 2'b01;
+    assign M_DATA_AXI_ARLOCK    = 1'b0;
+    assign M_DATA_AXI_ARCACHE   = 4'b0011;
+    assign M_DATA_AXI_ARPROT    = 3'h0;
+    assign M_DATA_AXI_ARQOS     = 4'h0;
+    assign M_DATA_AXI_ARUSER    = 'b0;
+    assign M_DATA_AXI_ARVALID   = 1'b0;    // *
+
+    // Rチャンネル
+    assign M_DATA_AXI_RREADY    = 1'b0;    // *
 
     /* ----- CPU状態 ----- */
     assign STAT = 8'b10101010;
