aac33cc7b688c4fd2662b86b5dde8d44e1db1b6c
NakagamiYuta <guguru0014@gmail.com>
2022-12-10 17:27:55 +0900

    [add](core) BRAM(4KB)作成

---
+++ cpu_core_1.0/src/bram_4kb/bram_4kb_stub.v
@@ -0,0 +1,26 @@
+// Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2019.1 (win64) Build 2552052 Fri May 24 14:49:42 MDT 2019
+// Date        : Sat Dec 10 17:26:18 2022
+// Host        : COJTHW110 running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub
+//               c:/Users/COJTHW110/COJT/cpu_proj/cpu_core_1.0/src/bram_4kb/bram_4kb_stub.v
+// Design      : bram_4kb
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z020clg484-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "blk_mem_gen_v8_4_3,Vivado 2019.1" *)
+module bram_4kb(clka, rsta, ena, wea, addra, dina, douta)
+/* synthesis syn_black_box black_box_pad_pin="clka,rsta,ena,wea[3:0],addra[31:0],dina[31:0],douta[31:0]" */;
+  input clka;
+  input rsta;
+  input ena;
+  input [3:0]wea;
+  input [31:0]addra;
+  input [31:0]dina;
+  output [31:0]douta;
+endmodule
